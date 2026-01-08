dd2ba836916a0fb2fda8bca232190c1e8516c8fc
Dharmik Patel <patel93@sheridancollege.ca>
2026-01-08 00:03:05 -0500

    trying to deploy

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,41 @@
+import Work1 from "../../assets/hangman.png";
+import Work2 from "../../assets/flappybird.png";
+import Work3 from "../../assets/X and O.png";
+import Work4 from "../../assets/image4.png";
+
+const Menu = [
+	{
+		id: 1,
+		image: Work1 ,
+		title: "Hangman Game",
+		category: ["Hangman game",],
+		repositoryUrl: "https://github.com/dharmik0711/python/tree/main/Hangman_game"
+	},
+
+	{
+		id: 2,
+		image: Work3,
+		title: "X and O",
+		category: ["X and O"],
+		url: "",
+		repositoryUrl: "https://github.com/dharmik0711/X-and-O-game-"
+	},
+	{
+		id: 3,
+		image: Work2,
+		title: "Flappy bird clone",
+		category: ["Flappy bird clone", "React"],
+		url: "",
+		repositoryUrl: "https://github.com/dharmik0711/Flappy_Bird"
+	},
+	{
+		id: 4,
+		image: Work4,
+		title: "Portfolio",
+		category: ["My Portfolio"],
+		url: "",
+		repositoryUrl: "https://github.com/dharmik0711/Portfolio_1"
+	}
+];
+
+export default Menu;
---
+++ src/components/resume/WorkExperience.jsx
@@ -0,0 +1,51 @@
+const WorkExperience = [
+    {
+        id: 1,
+        title: "Computer Systems Technology – Software Development and Network Engineering",
+        company: "Sheridan college",
+        yearsActive: "January 2024 - Present",
+        information: [
+            "Proficient in software development, including application design, coding, and debugging across various programming languages",
+            "Skilled in network engineering, with experience in configuring secure networks and managing system infrastructures"],
+
+
+
+
+
+     
+    },    
+   
+    
+    {
+        id: 3,
+        title: "HTMl and Css in depth ",
+        company: "Meta certified",
+        yearsActive: "Sep 2023",
+        information: [
+            "Create a simple form with a responsive layout using HTML5 and CSS",
+
+"Create a responsive layout using CSS ",
+
+"Create a UI using Bootstrap",
+
+"Implement debugging tools",
+            "Worked well independently and on a team to solve problems."
+        ],
+    },
+    {
+        id: 4,
+        title: "C for Everyone Programming Fundamentals",
+        company: "University of California, Santa Cruz",
+        yearsActive: "Sep 2023",
+        information: [
+           " Write a simple program.",
+
+"Compile, debug, and run a program.",
+
+"Apply concepts related to arrays & pointers, functions & storage classes, logic operators & various question types, lexical elements & data types"],
+    },
+    
+];
+
+
+export default WorkExperience;
